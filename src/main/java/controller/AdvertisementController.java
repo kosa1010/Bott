1cb046f65d1e29f80b8d633e7a572ed1a4e16085
kosa1010 <kosa1010@o2.pl>
2016-12-27 21:01:37 +0100

    Optymalizacja metody pobierającej opis ogłoszenia

--- src/main/java/controller/AdvertisementController.java
+++ src/main/java/controller/AdvertisementController.java
@@ -1,11 +1,8 @@
 package controller;
 
-import org.jsoup.Connection;
-import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 
-import java.io.IOException;
 
 /**
  * Created by kosa1010 on 11.12.16.
@@ -21,16 +18,11 @@ public class AdvertisementController {
         this.URL = URL;
     }
 
-    public String getDescription() throws IOException {
+    public String getDescription(Document document) {
         System.out.println("AdvertisementController.getDescription");
-        Connection connect = Jsoup.connect(URL).timeout(10 * 1000);
-        Document document = connect.get();
         Elements all = document.getElementsByAttribute("data-text");
         // if (all.get(0).getElementsByClass("bigImage").attr("data-nr") != null)
         String description = all.get(2).html().toString();//text());//getElementsByClass("photo-item").select("img").get(0).attr("src"));
-
-        //System.out.println(description);
-
         return description;
     }
 
